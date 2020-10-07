3616e5d07168adedadc7dfcc561e2a0c4ecb9058
파랑이 <parangee9706@gmail.com>
2020-10-07 06:13:22 +0000

    wa3

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -6,7 +6,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import { Link } from 'react-router-dom';
 
 const NavItem = (props) => (
-    <ListItem button as={Link} to = {props.href} component = 'a' key = {props.name}>
+    <ListItem button to = {props.to} component = 'a' key = {props.name}>
         <ListItemIcon>{props.icon}</ListItemIcon>
         <ListItemText primary = {props.name} style = {{color: 'rgba(0, 0, 0, 0.54)'}} />
     </ListItem>
